0b96fe0feb7deb314026018150d2e3da228e1197
quentinPitalier <quentin.pitalier@gmail.com>
2017-11-25 16:03:44 +0200

    Initial commit, include usefull ressources for mappa
    Pyrebase
    Firebase Web starter
    Web credentials for firebase
    Pepper first draws

---
+++ web/index.php
@@ -0,0 +1,18 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCzdHfBdkE_TuMO5Tf506QhlQBBjGD9wyM",
+    authDomain: "mappa-29c30.firebaseapp.com",
+    databaseURL: "https://mappa-29c30.firebaseio.com",
+    projectId: "mappa-29c30",
+    storageBucket: "",
+    messagingSenderId: "278379631937"
+  };
+  firebase.initializeApp(config);
+</script>
+
+<html>
+Hello World!
+</html>
